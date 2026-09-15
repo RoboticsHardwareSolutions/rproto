e71a8d88aff75e7600ffc5f721fddc5e903d2122
DocPillman <doc.pillman@gmail.com>
2026-09-15 16:26:26 +0300

    fomat fix

--- rproto.c
+++ rproto.c
@@ -1,23 +1,18 @@
 #include "rproto.h"
-#include "rcrc.h"
-#include "rserial.h"
-#include "rlog.h"
 #include "rbase64.h"
+#include "rcrc.h"
+#include "rlog.h"
+#include "rserial.h"
 #include "rtimeout.h"
 
-bool rproto_serial_start(rproto_serial* instance)
-{
-    if (instance == NULL)
-    {
+bool rproto_serial_start(rproto_serial *instance) {
+    if (instance == NULL) {
         return false;
     }
-    if (rserial_open(&instance->serial,
-                     instance->settings.port_name,
-                     instance->settings.baud,
-                     instance->settings.mode,
+    if (rserial_open(&instance->serial, instance->settings.port_name,
+                     instance->settings.baud, instance->settings.mode,
                      instance->settings.flow_ctrl,
-                     RPROTO_SERIAL_BYTE_TIMEOUT_US) != 0)
-    {
+                     RPROTO_SERIAL_BYTE_TIMEOUT_US) != 0) {
         RLOG_ERROR("rs serial : %s not started ", instance->settings.port_name);
         return false;
     }
@@ -27,101 +22,89 @@ bool rproto_serial_start(rproto_serial* instance)
     return true;
 }
 
-bool serial_get_preamble(rproto_serial* instance, unsigned int timeout_ms)
-{
-    uint8_t* preamble = (uint8_t*) &instance->buf.preamble;
-    int      res      = rserial_read(&instance->serial, preamble, sizeof(instance->buf.preamble), timeout_ms * 1000);
-    if (res < (int) sizeof(instance->buf.preamble))
-    {
+bool serial_get_preamble(rproto_serial *instance, unsigned int timeout_ms) {
+    uint8_t *preamble = (uint8_t *) &instance->buf.preamble;
+    int res = rserial_read(&instance->serial, preamble,
+                           sizeof(instance->buf.preamble), timeout_ms * 1000);
+    if (res < (int) sizeof(instance->buf.preamble)) {
         return false;
-    }
-    else
-    {
-        if (instance->buf.preamble == PREAMBLE_REQUEST || instance->buf.preamble == PREAMBLE_UNIQUE_ID ||
-            instance->buf.preamble == PREAMBLE_UNIQUE_ID || instance->buf.preamble == PREAMBLE_SET_ID ||
-            instance->buf.preamble == PREAMBLE_BROADCAST)
-        {
+    } else {
+        if (instance->buf.preamble == PREAMBLE_REQUEST ||
+            instance->buf.preamble == PREAMBLE_UNIQUE_ID ||
+            instance->buf.preamble == PREAMBLE_UNIQUE_ID ||
+            instance->buf.preamble == PREAMBLE_SET_ID ||
+            instance->buf.preamble == PREAMBLE_BROADCAST) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 }
 
-bool serial_get_id(rproto_serial* instance, unsigned int timeout_ms)
-{
-    int res = rserial_read(&instance->serial, &instance->buf.id, sizeof(instance->buf.id), timeout_ms * 1000);
-    if (res < (int) sizeof(instance->buf.id))
-    {
+bool serial_get_id(rproto_serial *instance, unsigned int timeout_ms) {
+    int res = rserial_read(&instance->serial, &instance->buf.id,
+                           sizeof(instance->buf.id), timeout_ms * 1000);
+    if (res < (int) sizeof(instance->buf.id)) {
         return false;
     }
     return true;
 }
 
-bool serial_get_proto_message_length(rproto_serial* instance, unsigned int timeout_ms)
-{
-    int res = rserial_read(&instance->serial,
-                           &instance->buf.payload_length,
-                           sizeof(instance->buf.payload_length),
-                           timeout_ms * 1000);
-
-    if (res < (int) sizeof(instance->buf.payload_length))
-    {
-        return false;
-    }
-
-    return true;
-}
-
-bool serial_get_payload(rproto_serial* instance, unsigned int timeout_ms)
-{
-    int res = rserial_read(&instance->serial, instance->buf.payload, instance->buf.payload_length, timeout_ms * 1000);
-
-    if (res < (int) instance->buf.payload_length)
-    {
-        return false;
-    }
-    return true;
-}
-
-bool serial_get_crc(rproto_serial* instance, unsigned int timeout_ms)
-{
+bool serial_get_proto_message_length(rproto_serial *instance,
+                                     unsigned int timeout_ms) {
     int res =
-        rserial_read(&instance->serial, (uint8_t*) &instance->buf.crc, sizeof(instance->buf.crc), timeout_ms * 1000);
+            rserial_read(&instance->serial, &instance->buf.payload_length,
+                         sizeof(instance->buf.payload_length), timeout_ms * 1000);
 
-    if (res < (int) sizeof(instance->buf.crc))
-    {
+    if (res < (int) sizeof(instance->buf.payload_length)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool serial_get_payload(rproto_serial *instance, unsigned int timeout_ms) {
+    int res = rserial_read(&instance->serial, instance->buf.payload,
+                           instance->buf.payload_length, timeout_ms * 1000);
+
+    if (res < (int) instance->buf.payload_length) {
         return false;
     }
     return true;
 }
 
-void encode_payload_and_copy_to(rproto_packet* src_packet, rproto_packet* dest_packet)
-{
-    dest_packet->payload_length =
-        (uint8_t) b64_decode(src_packet->payload, src_packet->payload_length, dest_packet->payload);
+bool serial_get_crc(rproto_serial *instance, unsigned int timeout_ms) {
+    int res = rserial_read(&instance->serial, (uint8_t *) &instance->buf.crc,
+                           sizeof(instance->buf.crc), timeout_ms * 1000);
+
+    if (res < (int) sizeof(instance->buf.crc)) {
+        return false;
+    }
+    return true;
 }
 
-void decode_payload_and_copy_to(rproto_packet* src_packet, rproto_packet* dest_packet)
-{
-    dest_packet->payload_length =
-        (uint8_t) b64_encode(src_packet->payload, src_packet->payload_length, dest_packet->payload);
+void encode_payload_and_copy_to(rproto_packet *src_packet,
+                                rproto_packet *dest_packet) {
+    dest_packet->payload_length = (uint8_t) b64_decode(
+            src_packet->payload, src_packet->payload_length, dest_packet->payload);
 }
 
-uint16_t computing_crc_of_packet(rproto_packet* packet)
-{
-    size_t size =
-        sizeof(packet->preamble) + sizeof(packet->id) + sizeof(packet->payload_length) + packet->payload_length;
-    uint16_t result = crc16_ccitt((char*) packet, (int) size);
+void decode_payload_and_copy_to(rproto_packet *src_packet,
+                                rproto_packet *dest_packet) {
+    dest_packet->payload_length = (uint8_t) b64_encode(
+            src_packet->payload, src_packet->payload_length, dest_packet->payload);
+}
+
+uint16_t computing_crc_of_packet(rproto_packet *packet) {
+    size_t size = sizeof(packet->preamble) + sizeof(packet->id) +
+                  sizeof(packet->payload_length) + packet->payload_length;
+    uint16_t result = crc16_ccitt((char *) packet, (int) size);
     return result;
 }
 
-int rproto_serial_setup(rproto_serial* instance, char* port_name, int baud, char* mode, int flowctrl)
-{
-    if (instance == NULL || port_name == NULL || baud == 0 || mode == NULL)
-    {
+int rproto_serial_setup(rproto_serial *instance, char *port_name, int baud,
+                        char *mode, int flowctrl) {
+    if (instance == NULL || port_name == NULL || baud == 0 || mode == NULL) {
         return -1;
     }
     strcpy(instance->settings.port_name, port_name);
@@ -131,48 +114,42 @@ int rproto_serial_setup(rproto_serial* instance, char* port_name, int baud, char
     return 0;
 }
 
-bool rproto_serial_get_packet(rproto_serial* instance, rproto_packet* packet, unsigned int timeout_ms)
-{
-    if (instance == NULL || packet == NULL)
-    {
+bool rproto_serial_get_packet(rproto_serial *instance, rproto_packet *packet,
+                              unsigned int timeout_ms) {
+    if (instance == NULL || packet == NULL) {
         return false;
     }
     bool got_preamble = serial_get_preamble(instance, timeout_ms);
-    if (!got_preamble)
-    {
+    if (!got_preamble) {
         return false;
     }
 
     bool got_client_id = serial_get_id(instance, timeout_ms);
-    if (!got_client_id)
-    {
+    if (!got_client_id) {
         return false;
     }
 
     /** get response */
-    bool got_client_id_and_length = serial_get_proto_message_length(instance, timeout_ms);
-    if (!got_client_id_and_length)
-    {
+    bool got_client_id_and_length =
+            serial_get_proto_message_length(instance, timeout_ms);
+    if (!got_client_id_and_length) {
         return false;
     }
 
     bool got_proto_payload = serial_get_payload(instance, timeout_ms);
-    if (!got_proto_payload)
-    {
+    if (!got_proto_payload) {
         return false;
     }
 
     /** get crc */
     bool got_crc = serial_get_crc(instance, timeout_ms);
-    if (!got_crc)
-    {
+    if (!got_crc) {
         return false;
     }
 
     uint16_t crc = computing_crc_of_packet(&instance->buf);
 
-    if (instance->buf.crc != crc)
-    {
+    if (instance->buf.crc != crc) {
         RLOG_ERROR("invalid crc ");
         return false;
     }
@@ -182,49 +159,44 @@ bool rproto_serial_get_packet(rproto_serial* instance, rproto_packet* packet, un
     return true;
 }
 
-bool rproto_serial_send_packet(rproto_serial* instance, rproto_packet* packet)
-{
-    if (instance == NULL || packet == NULL)
-    {
+bool rproto_serial_send_packet(rproto_serial *instance, rproto_packet *packet) {
+    if (instance == NULL || packet == NULL) {
         return false;
     }
-    if (packet->payload_length > PROTO_MAX_PAYLOAD_LENGTH)
-    {
-        RLOG_ERROR("payload length is too long MAX VALUE is %d", PROTO_MAX_PAYLOAD_LENGTH);
+    if (packet->payload_length > PROTO_MAX_PAYLOAD_LENGTH) {
+        RLOG_ERROR("payload length is too long MAX VALUE is %d",
+                   PROTO_MAX_PAYLOAD_LENGTH);
         return false;
     }
 
-    uint8_t        base64_payload_packet[sizeof(rproto_packet)];
-    rproto_packet* base64_packet = (rproto_packet*) base64_payload_packet;
+    uint8_t base64_payload_packet[sizeof(rproto_packet)];
+    rproto_packet *base64_packet = (rproto_packet *) base64_payload_packet;
 
-    memcpy(base64_payload_packet, (uint8_t*) packet, sizeof(packet->preamble) + sizeof(packet->id));
+    memcpy(base64_payload_packet, (uint8_t *) packet,
+           sizeof(packet->preamble) + sizeof(packet->id));
     decode_payload_and_copy_to(packet, base64_packet);
 
-    size_t size =
-        sizeof(packet->preamble) + sizeof(packet->id) + sizeof(packet->payload_length) + base64_packet->payload_length;
+    size_t size = sizeof(packet->preamble) + sizeof(packet->id) +
+                  sizeof(packet->payload_length) + base64_packet->payload_length;
 
-    uint16_t* crc = (uint16_t*) &base64_payload_packet[size];
-    *crc          = crc16_ccitt((char*) &base64_payload_packet, (int) size);
+    uint16_t *crc = (uint16_t *) &base64_payload_packet[size];
+    *crc = crc16_ccitt((char *) &base64_payload_packet, (int) size);
     size += sizeof(packet->crc);
     packet->crc = *crc;
 
     int res = rserial_write(&instance->serial, base64_payload_packet, size);
-    if (res < (int) size)
-    {
+    if (res < (int) size) {
         return false;
     }
     return true;
 }
 
-bool rproto_serial_is_ok(rproto_serial* instance)
-{
+bool rproto_serial_is_ok(rproto_serial *instance) {
     return rserial_is_ok(&instance->serial);
 }
 
-bool rproto_serial_stop(rproto_serial* instance)
-{
-    if (instance == NULL)
-    {
+bool rproto_serial_stop(rproto_serial *instance) {
+    if (instance == NULL) {
         return false;
     }
 #ifdef RPROTO_SERIAL_DEBUG
